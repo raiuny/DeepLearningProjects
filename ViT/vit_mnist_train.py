37de9b2cf26416fdf5b71903f01167cac7aeb7de
raiuny <793872973@qq.com>
2022-10-23 11:58:32 +0800

    modify train.py num_works>0

--- ViT/vit_mnist_train.py
+++ ViT/vit_mnist_train.py
@@ -8,28 +8,21 @@ from torchvision.datasets import MNIST
 from vit import ViT
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
-shutil.rmtree('./ViT/log', ignore_errors=True)
 
-train = MNIST(root='../datasets/mnist/', train=True, transform=transforms.ToTensor(), download=True)
-test = MNIST(root='../datasets/mnist/', train=False, transform=transforms.ToTensor(), download=True)
-trainloader = DataLoader(train, batch_size=50, shuffle= True, num_workers=0)
-testloader = DataLoader(test, batch_size=50, shuffle=False, num_workers=0)
-
-trainwriter = SummaryWriter(log_dir='./ViT/log/train',filename_suffix='_train')
-testwriter = SummaryWriter(log_dir='./ViT/log/test',filename_suffix='_test')
-
-def train_epoch(model, dataloader, criterion, optimizer, scheduler, epoch, device):
+def train_epoch(model, dataloader, writer, criterion, optimizer, scheduler, epoch, device):
     model.train()
     bar = tqdm(dataloader)
     bar.set_description(f'epoch: {epoch:2}')
     corr, total = 0, 0
-    for X, y in bar:
+    total_loss = 0
+    for i, (X, y) in enumerate(bar):
         X, gt_cls = X.to(device), y.to(device)
         pred_cls = model(X)
         loss = criterion(pred_cls, gt_cls)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        acc = (torch.argmax(pred_cls, axis=1)==gt_cls).sum().item()/len(X)*100
         corr += (torch.argmax(pred_cls, axis=1)==gt_cls).sum().item()
         total += len(X)
         bar.set_postfix_str(
@@ -37,11 +30,15 @@ def train_epoch(model, dataloader, criterion, optimizer, scheduler, epoch, devic
             f' acc={corr/total*100:.2f}'
             f' loss={loss.item():.2f}'
             )
-    trainwriter.add_scalar(tag='loss', scalar_value=loss.item(), global_step=epoch)
-    trainwriter.add_scalar(tag='acc', scalar_value=corr/total*100, global_step=epoch)
+        total_loss += loss.item()
+        writer.add_scalar(tag='loss_step', scalar_value=loss.item(), global_step=epoch*len(bar) + i)
+        writer.add_scalar(tag='acc_step', scalar_value=acc, global_step=epoch*len(bar) + i)
+    writer.add_scalar(tag='loss', scalar_value=total_loss/len(bar), global_step=epoch)
+    writer.add_scalar(tag='acc', scalar_value=corr/total*100, global_step=epoch)
+
     scheduler.step()
 
-def test_epoch(model, dataloader, device):
+def test_epoch(model, dataloader, writer, device):
     model.eval()
     with torch.no_grad():
         correct, total = 0, 0
@@ -51,10 +48,16 @@ def test_epoch(model, dataloader, device):
             correct += (torch.argmax(pred_cls, axis=1)==gt_cls).sum().item()
             total += len(X)
     print(f' val acc: {correct / total * 100:.2f}')
-    testwriter.add_scalar(tag='acc', scalar_value=correct/total*100, global_step=epoch)
+    writer.add_scalar(tag='acc', scalar_value=correct/total*100, global_step=epoch)
 
 if __name__ == '__main__':
-    
+    shutil.rmtree('./ViT/log', ignore_errors=True)
+    train = MNIST(root='../datasets/mnist/', train=True, transform=transforms.ToTensor(), download=True)
+    test = MNIST(root='../datasets/mnist/', train=False, transform=transforms.ToTensor(), download=True)
+    trainloader = DataLoader(train, batch_size=50, shuffle= True, num_workers=4)
+    testloader = DataLoader(test, batch_size=50, shuffle=False, num_workers=4)  
+    trainwriter = SummaryWriter(log_dir='./ViT/log/train',filename_suffix='_train')
+    testwriter = SummaryWriter(log_dir='./ViT/log/test',filename_suffix='_test')
     model = ViT(
     image_size=28, 
     channels=1,
@@ -76,6 +79,6 @@ if __name__ == '__main__':
     optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98, last_epoch=-1)
     for epoch in range(epoch_num):
-        train_epoch(model, trainloader, criterion, optimizer, scheduler, epoch, device)
-        test_epoch(model, testloader, device)
+        train_epoch(model, trainloader,trainwriter, criterion, optimizer, scheduler, epoch, device)
+        test_epoch(model, testloader, testwriter, device)
 
